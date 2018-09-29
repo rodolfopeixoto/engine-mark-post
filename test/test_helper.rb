f05d21e43177950d6905b3ae429e81788354b016
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2018-09-29 08:09:03 -0300

    Set up test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,7 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "mark"
+require 'minitest/unit'
+require 'minitest/autorun'
+require 'minitest/pride'
 
 require "minitest/autorun"
